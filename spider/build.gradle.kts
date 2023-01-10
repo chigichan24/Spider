b581f346e420773e231195140dd6f7104f1607ae
Kazuki Chigita <kazuki.chigichan@gmail.com>
2023-01-11 06:34:12 +0900

    Add integration test

--- spider/build.gradle.kts
+++ spider/build.gradle.kts
@@ -9,4 +9,6 @@ dependencies {
     testImplementation("org.mockito:mockito-inline:4.5.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.9")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
 }
