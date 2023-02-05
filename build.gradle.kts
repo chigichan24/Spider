66e345e9000467243549d49700daa10604b1441f
Kazuki Chigita <kazuki.chigichan@gmail.com>
2023-02-05 13:51:16 +0900

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     val kotlinVersion = "1.7.21"
-    id("com.android.application") version "8.0.0-alpha10" apply false
-    id("com.android.library") version "8.0.0-alpha10" apply false
+    id("com.android.application") version "8.1.0-alpha02" apply false
+    id("com.android.library") version "8.1.0-alpha02" apply false
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
     kotlin("jvm") version "1.7.21" apply false
 }
